94fec308b9841b99f512b5e26c696bd159b5d495
keerthananchowdary21 <skeerthana521@gmail.com>
2025-01-05 14:05:35 +0000

    code

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const sqlite3 = require('sqlite3')
 const path = require('path')
 const isValid = require('date-fns/isValid')
 const format = require('date-fns/format')
-const cors = require('cors')
+// const cors = require('cors')
 
 let db = null
 const app = express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 const dbPath = path.join(__dirname, 'todoApplication.db')
 const initializeDbAndServer = async () => {
   try {
